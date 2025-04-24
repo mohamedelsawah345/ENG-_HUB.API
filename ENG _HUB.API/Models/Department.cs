2af1d3885b275f743d136781be0493ad4686afbe
عمر 31157247 <UG_31157247@f-eng.tanta.edu.eg>
2025-04-24 16:57:09 +0200

    add models , Dbcontext and fluent api

---
+++ ENG _HUB.API/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace ENG__HUB.API.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+
+        public ICollection<User> Users { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+
+}
---
+++ ENG _HUB.API/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace ENG__HUB.API.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfilePhoto { get; set; }
+        public string Major { get; set; }
+        public string Role { get; set; }
+
+        public int DepartmentID { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<ToDoList> ToDoLists { get; set; }
+        public ICollection<Note> Notes { get; set; }
+        public ICollection<PromptHistory> PromptHistories { get; set; }
+        public ICollection<Course> Courses { get; set; } // many-to-many
+    }
+
+}
